d4132754b270078cd81ca0ba72ce8e01080a7001
Ethan Isola <isolaethan@gmail.com>
2022-01-26 18:39:24 -0600

    commit tree 664124f49726c049d4e71350ad82b6c928fa2459

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const seenNumbers = new Set(); // initialize an empty Set
+  for (const number of array) {
+    const complement = target - number;
+
+    // .has returns true if the Set includes the complement
+    if (seenNumbers.has(complement)) return true;
+
+    // .add adds the number to the Set
+    seenNumbers.add(number);
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  0(n)
 */
 
 /* 
   Add your pseudocode here
+  create a new set of numbers
+  for every number in the array create a new variable = to the target minus the number
+  if the set of numbers has the complement, return true and add it to the set
+  else return false
 */
 
 /*
   Add written explanation of your solution here
+  the function takes an array of numbers and a target number. A new set is created and then
+  it loops through the numbers and subtracts the number from the target value and adds it to the set.
+  If the set contains the solution of the target minus the number, it means that those two numbers
+  will add up to the target, so the function returns true. Else, it returns false
+
 */
 
 // You can run `node index.js` to view these console logs
